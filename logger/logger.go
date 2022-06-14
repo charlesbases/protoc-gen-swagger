86ee833801fee70243c658caf27ed9704ef4cc29
zhiming.sun <zhiming.sun@qq.com>
2022-06-14 14:40:22 +0800

    up logger

--- logger/logger.go
+++ logger/logger.go
@@ -9,11 +9,13 @@ import (
 // Fatal .
 func Fatal(v ...interface{}) {
 	os.Stderr.WriteString(colors.RedSprint(v...))
+	os.Stderr.WriteString("\n")
 	os.Exit(1)
 }
 
 // Fatalf .
 func Fatalf(format string, v ...interface{}) {
 	os.Stderr.WriteString(colors.RedSprintf(format, v...))
+	os.Stderr.WriteString("\n")
 	os.Exit(1)
 }
